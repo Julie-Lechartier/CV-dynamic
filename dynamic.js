89017df05fa20074c18322b2d3875a14b2f4e92c
Julie_LECHARTIER <julie.lechartier@gmail.com>
2023-12-05 09:19:53 +0100

    ajout du javascript

---
+++ dynamic.js
@@ -0,0 +1,216 @@
+// Partie Compétence
+class Compt1{
+    constructor(Compt1){
+        this.Compt1 = Compt1;
+    }
+}
+class Compt2{
+    constructor(Compt2){
+        this.Compt2 = Compt2;
+    }
+}
+class Compt3{
+    constructor(Compt3){
+        this.Compt3 = Compt3;
+
+    }
+}
+
+class Compt4{
+    constructor(Compt4){
+        this.Compt4 = Compt4;
+
+    }
+}
+
+
+//Partie Langue
+
+class Lang1{
+    constructor(Lang1){
+        this.Lang1 = Lang1
+    }
+
+}
+class Lang2{
+    constructor(Lang2){
+        this.Lang2 = Lang2
+    }
+
+}
+
+//Partie Soft Skill
+
+class Ss1{
+    constructor(Ss1){
+        this.Ss1 = Ss1
+    }
+}
+
+class Ss2{
+    constructor(Ss2){
+        this.Ss2 = Ss2
+    }
+}
+
+class Ss3{
+    constructor(Ss3){
+        this.Ss3 = Ss3
+    }
+}
+
+class Ss4{
+    constructor(Ss4){
+        this.Ss4 = Ss4
+    }
+}
+
+//Partie Expérience professionnel
+
+//travail 1 
+class titrePro1{
+    constructor(titrePro1){
+        this.titrePro1 = titrePro1
+    }
+}
+
+class entreprise1{
+    constructor(entreprise1){
+        this.entreprise1 = entreprise1
+    }
+}
+
+class lieu1{
+    constructor(lieu1){
+        this.lieu1 = lieu1
+    }
+} 
+class poste1{
+    constructor(poste1){
+        this.poste1 = poste1
+    }
+}
+class periode1{
+    constructor(periode1){
+        this.periode1 = periode1
+    }
+}
+
+const us1TitrePro1 = new titrePro1()
+const us1Entreprise1 = new entreprise1()
+const us1Lieu1 = new lieu1()
+const us1Poste1 = new poste1()
+const us1Periode1 = new periode1()
+
+document.getElementById("titrePro1").innerHTML = us1TitrePro1
+document.getElementById("entreprise1").innerHTML = us1Entreprise1
+document.getElementById("lieu1").innerHTML = us1Lieu1
+document.getElementById("poste1").innerHTML = us1Poste1
+document.getElementById("periode1").innerHTML = us1Periode1
+
+// travail 2 
+
+class titrePro2{
+    constructor(titrePro2){
+        this.titrePro2 = titrePro2
+    }
+}
+
+class entreprise2{
+    constructor(entreprise2){
+        this.entreprise2 = entreprise2
+    }
+}
+
+class lieu2{
+    constructor(lieu2){
+        this.lieu2 = lieu2
+    }
+} 
+class poste2{
+    constructor(poste2){
+        this.poste2 = poste2
+    }
+}
+class periode2{
+    constructor(periode2){
+        this.periode2 = periode2
+    }
+}
+
+const us1TitrePro2 = new titrePro2()
+const us1Entreprise2 = new entreprise2()
+const us1Lieu2 = new lieu2()
+const us1Poste2 = new poste2()
+const us1Periode2 = new periode2()
+
+document.getElementById("titrePro2").innerHTML = us1TitrePro2
+document.getElementById("entreprise2").innerHTML = us1Entreprise2
+document.getElementById("lieu2").innerHTML = us1Lieu2
+document.getElementById("poste2").innerHTML = us1Poste2
+document.getElementById("periode2").innerHTML = us1Periode2
+
+//travail 3
+class titrePro3{
+    constructor(titrePro3){
+        this.titrePro3 = titrePro3
+    }
+}
+
+class entreprise3{
+    constructor(entreprise3){
+        this.entreprise3 = entreprise3
+    }
+}
+
+class lieu3{
+    constructor(lieu3){
+        this.lieu3 = lieu3
+    }
+} 
+class poste3{
+    constructor(poste3){
+        this.poste3 = poste3
+    }
+}
+class periode3{
+    constructor(periode3){
+        this.periode3 = periode3
+    }
+}
+
+const us1TitrePro3 = new titrePro3()
+const us1Entreprise3 = new entreprise3()
+const us1Lieu3 = new lieu3()
+const us1Poste3 = new poste3()
+const us1Periode3 = new periode3()
+
+document.getElementById("titrePro3").innerHTML = us1TitrePro3
+document.getElementById("entreprise3").innerHTML = us1Entreprise3
+document.getElementById("lieu3").innerHTML = us1Lieu3
+document.getElementById("poste3").innerHTML = us1Poste3
+document.getElementById("periode3").innerHTML = us1Periode3
+// Utilisateur 1
+
+const us1Compt1 = new Compt1 ()
+const us1Compt2 = new Compt2 ()
+const us1Compt3 = new Compt3 ()
+const us1Compt4 = new Compt4 ()
+const us1Lang1 = new Lang1 ()
+const us1Lang2 = new Lang2 ()
+const us1Ss1 = new Ss1()
+const us1Ss2 = new Ss2()
+const us1Ss3 = new Ss3()
+const us1Ss4 = new Ss4()
+
+
+document.getElementById("Compt1").innerHTML = us1Compt1
+document.getElementById("Compt2").innerHTML = us1Compt2
+document.getElementById("Compt3").innerHTML = us1Compt3
+document.getElementById("Compt4").innerHTML = us1Compt4
+document.getElementById("Lang1").innerHTML = us1Lang1
+document.getElementById("Lang2").innerHTML = us1Lang2
+document.getElementById("Ss1").innerHTML = us1Ss1
+document.getElementById("Ss2").innerHTML = us1Ss2
+document.getElementById("Ss3").innerHTML = us1Ss3
+document.getElementById("Ss4").innerHTML = us1Ss4
