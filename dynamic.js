e70e46381e1fcfecb0573669982e7be717d3de1d
Julie_LECHARTIER <julie.lechartier@gmail.com>
2023-12-07 14:12:03 +0100

    ajout ID javascript sur HTML

--- dynamic.js
+++ dynamic.js
@@ -96,17 +96,6 @@ class periode1{
     }
 }
 
-const us1TitrePro1 = new titrePro1()
-const us1Entreprise1 = new entreprise1()
-const us1Lieu1 = new lieu1()
-const us1Poste1 = new poste1()
-const us1Periode1 = new periode1()
-
-document.getElementById("titrePro1").innerHTML = us1TitrePro1
-document.getElementById("entreprise1").innerHTML = us1Entreprise1
-document.getElementById("lieu1").innerHTML = us1Lieu1
-document.getElementById("poste1").innerHTML = us1Poste1
-document.getElementById("periode1").innerHTML = us1Periode1
 
 // travail 2 
 
@@ -138,17 +127,6 @@ class periode2{
     }
 }
 
-const us1TitrePro2 = new titrePro2()
-const us1Entreprise2 = new entreprise2()
-const us1Lieu2 = new lieu2()
-const us1Poste2 = new poste2()
-const us1Periode2 = new periode2()
-
-document.getElementById("titrePro2").innerHTML = us1TitrePro2
-document.getElementById("entreprise2").innerHTML = us1Entreprise2
-document.getElementById("lieu2").innerHTML = us1Lieu2
-document.getElementById("poste2").innerHTML = us1Poste2
-document.getElementById("periode2").innerHTML = us1Periode2
 
 //travail 3
 class titrePro3{
@@ -179,17 +157,6 @@ class periode3{
     }
 }
 
-const us1TitrePro3 = new titrePro3()
-const us1Entreprise3 = new entreprise3()
-const us1Lieu3 = new lieu3()
-const us1Poste3 = new poste3()
-const us1Periode3 = new periode3()
-
-document.getElementById("titrePro3").innerHTML = us1TitrePro3
-document.getElementById("entreprise3").innerHTML = us1Entreprise3
-document.getElementById("lieu3").innerHTML = us1Lieu3
-document.getElementById("poste3").innerHTML = us1Poste3
-document.getElementById("periode3").innerHTML = us1Periode3
 // Utilisateur 1
 
 const us1Compt1 = new Compt1 ()
@@ -202,7 +169,21 @@ const us1Ss1 = new Ss1()
 const us1Ss2 = new Ss2()
 const us1Ss3 = new Ss3()
 const us1Ss4 = new Ss4()
-
+const us1TitrePro1 = new titrePro1()
+const us1Entreprise1 = new entreprise1()
+const us1Lieu1 = new lieu1()
+const us1Poste1 = new poste1()
+const us1Periode1 = new periode1()
+const us1TitrePro2 = new titrePro2()
+const us1Entreprise2 = new entreprise2()
+const us1Lieu2 = new lieu2()
+const us1Poste2 = new poste2()
+const us1Periode2 = new periode2()
+const us1TitrePro3 = new titrePro3()
+const us1Entreprise3 = new entreprise3()
+const us1Lieu3 = new lieu3()
+const us1Poste3 = new poste3()
+const us1Periode3 = new periode3()
 
 document.getElementById("Compt1").innerHTML = us1Compt1
 document.getElementById("Compt2").innerHTML = us1Compt2
@@ -214,3 +195,18 @@ document.getElementById("Ss1").innerHTML = us1Ss1
 document.getElementById("Ss2").innerHTML = us1Ss2
 document.getElementById("Ss3").innerHTML = us1Ss3
 document.getElementById("Ss4").innerHTML = us1Ss4
+document.getElementById("titrePro1").innerHTML = us1TitrePro1
+document.getElementById("entreprise1").innerHTML = us1Entreprise1
+document.getElementById("lieu1").innerHTML = us1Lieu1
+document.getElementById("poste1").innerHTML = us1Poste1
+document.getElementById("periode1").innerHTML = us1Periode1
+document.getElementById("titrePro2").innerHTML = us1TitrePro2
+document.getElementById("entreprise2").innerHTML = us1Entreprise2
+document.getElementById("lieu2").innerHTML = us1Lieu2
+document.getElementById("poste2").innerHTML = us1Poste2
+document.getElementById("periode2").innerHTML = us1Periode2
+document.getElementById("titrePro3").innerHTML = us1TitrePro3
+document.getElementById("entreprise3").innerHTML = us1Entreprise3
+document.getElementById("lieu3").innerHTML = us1Lieu3
+document.getElementById("poste3").innerHTML = us1Poste3
+document.getElementById("periode3").innerHTML = us1Periode3
