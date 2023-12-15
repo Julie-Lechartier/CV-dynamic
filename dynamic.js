ef3b95b2efc3340d652a9e471769d89499794b4c
Julie_LECHARTIER <julie.lechartier@gmail.com>
2023-12-15 17:01:48 +0100

    changement html css et ajout liste js

--- dynamic.js
+++ dynamic.js
@@ -1,24 +1,45 @@
+//Partie Perso
+class mail{
+    constructor(unMail){
+        this.unMail = unMail;
+    }
+}
+class address{
+    constructor(uneAdsress){
+        this.uneAdsress = uneAdsress;
+    }
+}
+
+class tel{
+    constructor(num){
+        this.num = num;
+    }
+}
+
+
+
 // Partie Compétence
-class Compt1{
-    constructor(Compt1){
-        this.Compt1 = Compt1;
+class compt01{
+    constructor(compt1){
+        this.compt1 = compt1;
     }
 }
-class Compt2{
-    constructor(Compt2){
-        this.Compt2 = Compt2;
+
+class compt02{
+    constructor(compt2){
+        this.compt2 = compt2;
     }
 }
-class Compt3{
-    constructor(Compt3){
-        this.Compt3 = Compt3;
+class compt03{
+    constructor(compt3){
+        this.compt3 = compt3;
 
     }
 }
 
-class Compt4{
-    constructor(Compt4){
-        this.Compt4 = Compt4;
+class compt04{
+    constructor(compt4){
+        this.compt4 = compt4;
 
     }
 }
@@ -65,32 +86,33 @@ class Ss4{
     }
 }
 
+
 //Partie Expérience professionnel
 
 //travail 1 
-class titrePro1{
+class titrePro01{
     constructor(titrePro1){
         this.titrePro1 = titrePro1
     }
 }
 
-class entreprise1{
+class entreprise01{
     constructor(entreprise1){
         this.entreprise1 = entreprise1
     }
 }
 
-class lieu1{
+class lieu01{
     constructor(lieu1){
         this.lieu1 = lieu1
     }
 } 
-class poste1{
+class poste01{
     constructor(poste1){
         this.poste1 = poste1
     }
 }
-class periode1{
+class periode01{
     constructor(periode1){
         this.periode1 = periode1
     }
@@ -158,55 +180,73 @@ class periode3{
 }
 
 // Utilisateur 1
+    //liste
+const us1Mail = new mail("Henri.petit@sfr.fr")
+const us1Address = new address("Villeurbanne, Lyon")
+const us1Tel = new tel("06 05 07 81 50")
 
-const us1Compt1 = new Compt1 ()
-const us1Compt2 = new Compt2 ()
-const us1Compt3 = new Compt3 ()
-const us1Compt4 = new Compt4 ()
-const us1Lang1 = new Lang1 ()
-const us1Lang2 = new Lang2 ()
-const us1Ss1 = new Ss1()
-const us1Ss2 = new Ss2()
-const us1Ss3 = new Ss3()
-const us1Ss4 = new Ss4()
-const us1TitrePro1 = new titrePro1()
-const us1Entreprise1 = new entreprise1()
-const us1Lieu1 = new lieu1()
-const us1Poste1 = new poste1()
-const us1Periode1 = new periode1()
-const us1TitrePro2 = new titrePro2()
-const us1Entreprise2 = new entreprise2()
-const us1Lieu2 = new lieu2()
-const us1Poste2 = new poste2()
-const us1Periode2 = new periode2()
-const us1TitrePro3 = new titrePro3()
-const us1Entreprise3 = new entreprise3()
-const us1Lieu3 = new lieu3()
-const us1Poste3 = new poste3()
-const us1Periode3 = new periode3()
+const us1Compt1 = new compt01 ("HTML")
+const us1Compt2 = new compt02 ("CSS")
+const us1Compt3 = new compt03 ("Python")
+const us1Compt4 = new compt04 ("C")
+const us1Lang1 = new Lang1 ("Anglais")
+const us1Lang2 = new Lang2 ("Français")
+const us1Ss1 = new Ss1("Motivé")
+const us1Ss2 = new Ss2("Curieux")
+const us1Ss3 = new Ss3("Adaptable")
+const us1Ss4 = new Ss4("Concessieux")
 
-document.getElementById("Compt1").innerHTML = us1Compt1
-document.getElementById("Compt2").innerHTML = us1Compt2
-document.getElementById("Compt3").innerHTML = us1Compt3
-document.getElementById("Compt4").innerHTML = us1Compt4
-document.getElementById("Lang1").innerHTML = us1Lang1
-document.getElementById("Lang2").innerHTML = us1Lang2
-document.getElementById("Ss1").innerHTML = us1Ss1
-document.getElementById("Ss2").innerHTML = us1Ss2
-document.getElementById("Ss3").innerHTML = us1Ss3
-document.getElementById("Ss4").innerHTML = us1Ss4
-document.getElementById("titrePro1").innerHTML = us1TitrePro1
-document.getElementById("entreprise1").innerHTML = us1Entreprise1
-document.getElementById("lieu1").innerHTML = us1Lieu1
-document.getElementById("poste1").innerHTML = us1Poste1
-document.getElementById("periode1").innerHTML = us1Periode1
-document.getElementById("titrePro2").innerHTML = us1TitrePro2
-document.getElementById("entreprise2").innerHTML = us1Entreprise2
-document.getElementById("lieu2").innerHTML = us1Lieu2
-document.getElementById("poste2").innerHTML = us1Poste2
-document.getElementById("periode2").innerHTML = us1Periode2
-document.getElementById("titrePro3").innerHTML = us1TitrePro3
-document.getElementById("entreprise3").innerHTML = us1Entreprise3
-document.getElementById("lieu3").innerHTML = us1Lieu3
-document.getElementById("poste3").innerHTML = us1Poste3
-document.getElementById("periode3").innerHTML = us1Periode3
+
+let listContact = [us1Mail.unMail, us1Address.uneAdsress, us1Tel.num,];
+for (i=0; i<listContact.length; i++){
+    let elementListe = document.createElement("li");
+    elementListe.innerHTML = listContact[i]
+    document.getElementById("contact").appendChild(elementListe);
+}
+
+let listCompetence = [us1Compt1.compt1, us1Compt2.compt2, us1Compt3.compt3, us1Compt4.compt4]
+for (i=0; i<listCompetence.length; i++){
+    let liste = document.createElement("li");
+    liste.innerHTML = listCompetence[i];
+    document.getElementById("competences").appendChild(liste)
+}
+
+let listLangage = [us1Lang1.Lang1, us1Lang2.Lang2]
+for (i=0; i<listLangage.length; i++){
+    let lang = document.createElement("li");
+    lang.innerHTML = listLangage[i];
+    document.getElementById("langage").appendChild(lang)
+}
+
+let listSS = [us1Ss1.Ss1, us1Ss2.Ss2, us1Ss3.Ss3, us1Ss4.Ss4]
+for (i=0; i<listSS.length; i++){
+    let sofS = document.createElement("li");
+    sofS.innerHTML = listSS[i];
+    document.getElementById("softSkill").appendChild(sofS)
+}
+
+
+
+
+
+const us1TitrePro1 = new titrePro01("Vente")
+const us1Entreprise1 = new entreprise01("Carrefour")
+const us1Lieu1 = new lieu01("Lyon")
+const us1Poste1 = new poste01("tennue de caisse")
+const us1Periode1 = new periode01("12 décemnbre au 15 Janvier 2021")
+const us1TitrePro2 = new titrePro2("Vente")
+const us1Entreprise2 = new entreprise2("Zara")
+const us1Lieu2 = new lieu2("Lyon")
+const us1Poste2 = new poste2("Conseille de vente")
+const us1Periode2 = new periode2("Juin - Aout 2022")
+const us1TitrePro3 = new titrePro3("Boulangerie")
+const us1Entreprise3 = new entreprise3("Painaulait")
+const us1Lieu3 = new lieu3("Villeurbanne")
+const us1Poste3 = new poste3("Vente")
+const us1Periode3 = new periode3("Septembre 2023")
+
+
+document.getElementById("titrePro001").innerHTML = us1TitrePro1.titrePro1
+document.getElementById("Entreprise001").innerHTML = us1Entreprise1.entreprise1
+document.getElementById("lieu001").innerText = us1Lieu1.lieu1
+document.getElementById("poste001").innerHTML = us1Poste1.poste1
